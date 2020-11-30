c36f339e6922940a096e4662262c29e2dae17084
Pierre Rakotondrafara <prakoton@telecom-paris.fr>
2020-11-30 19:45:13 +0100

    chromagram preparation function started and almost done

--- prepare-data.py
+++ prepare-data.py
@@ -8,5 +8,25 @@ FIG_SIZE = (15,10)
 file = "Chill.mp3"
 
 # load audio file with Librosa
-signal, sample_rate = librosa.load(file, sr=22050)
+signal, sample_rate = librosa.load(file, sr=44100)
+#%% Data preparation constants
+nbOfFrames = 60
+sr = 44100
+binsPerOctave = 24
+frameSize = 8192
+hopLength = frameSize
 
+#%% Print data information
+
+hopLengthDuration = float(hopLength)/sr
+frameDuration = float(frameSize)/sr
+print("STFT hop length duration is: {}sc".format(hopLengthDuration))
+print("STFT window duration is: {}sc".format(frameDuration))
+
+#%% FUNCTION TO TRANSFORM A WAVEFORM INTO A CHROMAGRAM OF LENGTH 60 FRAMES
+def our_chromagram(signal):
+    l = len(signal)
+    offset = np.random.randint(l - 70*hopLength)
+    croppedSig = signal[offset: offset + 59*frameSize + 1]
+    cropSigDur = len(cropSigDur)/sr
+    return librosa.feature.chroma_stft(croppedSig, sr, n_fft= frameSize, hop_length = hopLength)
