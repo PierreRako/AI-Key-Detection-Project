7a032cd930b07bbd98ba1e72891c5d05d46464a4
Pierre Rakotondrafara <prakoton@telecom-paris.fr>
2020-11-30 16:03:55 +0100

    loading an ad-hoc file

--- prepare-data.py
+++ prepare-data.py
@@ -1,1 +1,12 @@
-import librosa;
+import numpy as np
+import librosa, librosa.display;
+import matplotlib.pyplot as plt
+print("hello world")
+
+FIG_SIZE = (15,10)
+
+file = "Chill.mp3"
+
+# load audio file with Librosa
+signal, sample_rate = librosa.load(file, sr=22050)
+
