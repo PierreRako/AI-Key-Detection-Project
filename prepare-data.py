6aaf1c85768b78f2161d12a5e05b1262b7a050fc
Pierre Rakotondrafara <prakoton@telecom-paris.fr>
2020-12-01 17:04:28 +0100

    Testing chroma function and commenting it

--- prepare-data.py
+++ prepare-data.py
@@ -24,9 +24,25 @@ print("STFT hop length duration is: {}sc".format(hopLengthDuration))
 print("STFT window duration is: {}sc".format(frameDuration))
 
 #%% FUNCTION TO TRANSFORM A WAVEFORM INTO A CHROMAGRAM OF LENGTH 60 FRAMES
+
+# output of this function is a 12*60 matrix which represents the chromgram 
+# of (randomly chosen) 11sc of an input song. This can be used to create a dataset 
+# that can be directly fed to any neural network.
+
 def our_chromagram(signal):
     l = len(signal)
     offset = np.random.randint(l - 70*hopLength)
     croppedSig = signal[offset: offset + 59*frameSize + 1]
-    cropSigDur = len(cropSigDur)/sr
+    cropSigDur = len(croppedSig)/sr
+    print("New signal duration : ", cropSigDur)
     return librosa.feature.chroma_stft(croppedSig, sr, n_fft= frameSize, hop_length = hopLength)
+chroma = our_chromagram(signal)
+print("dimension chromagramme : ", chroma.shape)
+librosa.display.specshow(chroma)
+plt.show()
+
+#%% FUNCTION TO TRANSFORM A WAVEFORM INTO A SPECTROGRAM WHICH IS LINEAR WITH RESPECT TO SEMITONES
+
+## 
+def our_spectrogram(signal):
+    return None
