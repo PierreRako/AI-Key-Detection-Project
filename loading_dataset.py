2519b71350841be5dc3460b2d3836f4b9738da41
Mirado <mirado.rabenasolo@telecom-paris.fr>
2020-12-03 22:20:04 +0100

    Preparation des tuples(audio, key) fonctionnelle

--- loading_dataset.py
+++ loading_dataset.py
@@ -8,40 +8,67 @@ import librosa, librosa.display
 cmap = plt.get_cmap('inferno')
 keys = 'A major, A minor, Ab major, Ab minor, B major, B minor, Bb minor, Bb major, C major, C minor, D major, D minor, Db minor, Db major, E minor, E major, Eb minor, Eb major, F minor, F major, G major, G minor, Gb minor, Gb major'.split(",")
 
-nbOfFrames = 60
-sr = 44100
-binsPerOctave = 24
-frameSize = 8192
-hopLength = frameSize
+# @return fileName without any extension
+def fileName_from_path(path):
+    fileNameFull = path.split("/")[-1]
+    splitName = fileNameFull.split(".")[:-1]
+    
+    separator = "."
+    fileName = separator.join(splitName)
+    return fileName
 
-hopLengthDuration = float(hopLength)/sr
-frameDuration = float(frameSize)/sr
+# @param files is a list of paths to files
+# @param keys is a list of pahts to key files
+def zip_audio_key(files, keys):
+    orderedKeyFiles = []
+
+    keyNames = [fileName_from_path(key) for key in keys]
+
+    for file in files:
+        fileName = fileName_from_path(file)
+
+        index = keyNames.index(fileName)
+
+        keyFile = open(keys[index], "r")
+        key = keyFile.read()
+
+        tuple = [file, key]
+        orderedKeyFiles.append(tuple)
+
+    return orderedKeyFiles
 
 # A function to load a dataset (folder containing all audios)
+# It associates the path of an audio to its corresponding key
 # @param data_set_path User has to specify the paf to the folder containing all audio
-# @return paths of each file in the dataset
-def load_dataset(data_set_path):
+# @param key_annotation_path
+# @return list of tuples [audio, key]
+def load_dataset(data_set_path, key_annotation_path):
     print(f"loading {data_set_path}")
+    #Getting all audio files
     files = glob.glob(os.path.join(data_set_path, '*.wav'))
-    return files
+
+    #Getting key_annotations
+    key_annotations = glob.glob(os.path.join(key_annotation_path, '*'))
+
+    audio_key_tuples = zip_audio_key(files, key_annotations)
+
+    return audio_key_tuples
 
 # A function to convert audio files of a dataset to chroma image and save it
 # @param data_set_path
 
-def convert_to_chroma_images(data_set_path):
-    files = load_dataset(data_set_path)
+def convert_to_chroma_images(data_set_path, key_annotation_path):
+    audio_key_tuples = load_dataset(data_set_path, key_annotation_path)
 
     i = 0
-    for file in files:
+    for tuple in audio_key_tuples:
         #Extracting the fileName from the path
-        fileNameWithExtension = file.split("/")[-1]
-        fileName = fileNameWithExtension.split(".")[0]
-        print(fileName)
+        fileName = fileName_from_path(tuple[0])
         
         if (i!=0):
             break
         
-        signal, sample_rate = librosa.load(file, sr=44100)
+        signal, sample_rate = librosa.load(tuple[0], sr=44100)
         chroma = prepare_data1(signal)
         plt.figure()
         librosa.display.specshow(chroma)
@@ -49,7 +76,11 @@ def convert_to_chroma_images(data_set_path):
         plt.savefig(f'img_data/{fileName}.png')
         plt.clf()
 
+        #Setting the path to the image
+        cwd = os.getcwd()
+        tuple[0] = glob.glob(os.path.abspath(f'{cwd}/img_data/{fileName}.png'))
+
+        print(tuple[0])
+        print(tuple[1])
         i+=1
     return 0
-
-convert_to_chroma_images("/home/mirado/GIT/AI-Key-Detection-Project/Datasets/giantsteps-key-dataset-master/audio")
