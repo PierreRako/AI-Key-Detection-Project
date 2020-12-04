20039a945c1ebbe9474d94a2c37b10bfb4f68398
Mirado <mirado.rabenasolo@telecom-paris.fr>
2020-12-04 20:00:48 +0100

    Découpage de la data en [nom, chroma, gamme, gamme_codée] fonctionnelle

--- global_main.py
+++ global_main.py
@@ -2,4 +2,4 @@ from loading_dataset import *
 
 dataset_path = "/home/mirado/GIT/AI-Key-Detection-Project/Datasets/giantsteps-key-dataset-master/audio"
 key_annotations_path = "/home/mirado/GIT/AI-Key-Detection-Project/Datasets/giantsteps-key-dataset-master/annotations/key"
-convert_to_chroma_images(dataset_path, key_annotations_path)
+prepare_panda_dataFrame(dataset_path, key_annotations_path)
