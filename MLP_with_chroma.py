69fb2547552524ac19a4dbedabb01dc47bc79f2d
Pierre Rakotondrafara <prakoton@telecom-paris.fr>
2020-12-05 19:11:45 +0100

    experimentation started  on neural net and sklearn

---
+++ MLP_with_chroma.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPClassifier
+
+# Dataset loading (panda) and separation
+
+# Neural network parameters
+
+epochs = 100
+learning_rate = 0.01
+hidden_layer_sizes = (100,50,30)
+activation = 'logistic'
+
+clf = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes,activation=activation,
+random_state=1,max_iter=epochs,learning_rate=learning_rate)
+
---
+++ test_sklearn.py
@@ -0,0 +1,14 @@
+from sklearn.neural_network import MLPClassifier
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+iris = datasets.load_iris()
+epochs=150
+learning_rate=0.04
+X, y = iris.data, iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=1)
+print(y_train)
+clf = MLPClassifier(hidden_layer_sizes=(10),random_state=1, max_iter=epochs).fit(X_train, y_train)
+print(MLPClassifier.)
+array1 = clf.predict_proba(X_test[:1])
+array2 = clf.predict(X_test[:5, :])
+array3 = clf.score(X_test, y_test)
