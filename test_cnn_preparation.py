7088cd39131c80ae404c416f9c82ad28a13cf49e
Pierre Rakotondrafara <prakoton@telecom-paris.fr>
2021-01-09 11:50:35 +0100

    complete test for the cnn data preparation

--- test_cnn_preparation.py
+++ test_cnn_preparation.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from prepare_CNN_data import prepare_cnn_data
+import librosa, librosa.display
 
 
-dataset_path = "./Datasets/giantsteps-key-dataset-master/audio"
-annotations_path = "./Datasets/giantsteps-key-dataset-master/annotations/key"
+dataset_path = "./test_dataset/audio"
+annotations_path = "./test_dataset/annotations"
 names, X, keys, y = prepare_cnn_data(dataset_path,annotations_path)
-print(X.shape)
+print("dataset shape : ", X.shape)
+print("labels shape : ", y.shape)
+print("first 3 labels :\n (coded) ", y[:3], "\n (raw) ", keys[:3] )
+librosa.display.specshow(X[0,:,:])
+plt.show()
