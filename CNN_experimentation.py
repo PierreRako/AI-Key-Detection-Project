1f00437f12e71e7537c98cc1eb399956b3515553
Pierre Rakotondrafara <prakoton@telecom-paris.fr>
2021-01-10 15:40:45 +0100

    completed cnn architecture

--- CNN_experimentation.py
+++ CNN_experimentation.py
@@ -23,7 +23,11 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
+X_train = X_train.reshape(-1,120,100,1)
+X_test = X_test.reshape(-1,120,100,1)
+
 model = keras.Sequential()
+model.add(keras.Input(shape = (120,100,1)))
 for _ in range(5):
     model.add(layers.Conv2D(
         8,
@@ -32,3 +36,24 @@ for _ in range(5):
         padding="same"
         ))
 
+model.summary()
+
+model.add(layers.Permute((2,1,3)))
+
+model.summary()
+
+model.add(layers.Reshape((100,-1)))
+
+model.add(layers.Dense(48,activation="relu"))
+
+model.summary()
+
+model.add(layers.GlobalAveragePooling1D())
+
+model.summary()
+
+model.add(layers.Dense(24,activation="softmax"))
+
+model.summary()
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
