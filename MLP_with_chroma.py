c663a8742c51d5c9cacd863cb92950cb95c07580
Mirado <mirado.rabenasolo@telecom-paris.fr>
2021-01-10 23:44:10 +0100

    Creation of the notebook for the optimization, little reorganisation on the MLP_with_chroma code

--- MLP_with_chroma.py
+++ MLP_with_chroma.py
@@ -6,18 +6,20 @@ from loading_dataset import prepare_panda_dataFrame
 import pandas as pd
 
 # Dataset loading and separation
-dataset_path="./Datasets/giantsteps-key-dataset-master/audio"
-key_annotation_path="./Datasets/giantsteps-key-dataset-master/annotations/key"
-chroma_df=prepare_panda_dataFrame(dataset_path,key_annotation_path)
+dataset_path = "./Datasets/giantsteps-key-dataset-master/audio"
+key_annotation_path = "./Datasets/giantsteps-key-dataset-master/annotations/key"
+chroma_df = prepare_panda_dataFrame(dataset_path,key_annotation_path)
 
 def network_training(learning_rate):
-    print("data loaded")
+    print("Data loaded")
     print(chroma_df.head(3))
     X, y = np.stack(chroma_df['chromagram']), np.array(chroma_df['coded_key'].astype('int'))
     print("shape of X and y : ", X.shape,y.shape)
+
     #print("first row of X : ", X[0,:20],y[0])
     X_train, X_test, y_train, y_test = train_test_split(X ,y ,test_size=0.2)
     print("data separated")
+
     # Neural network parameters
     epochs = 400
     iter_for_convergence = epochs
@@ -31,17 +33,24 @@ def network_training(learning_rate):
 
     # NNet init
     clf = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes, activation=activation, 
-    random_state=1, max_iter=epochs, learning_rate_init=learning_rate,learning_rate='adaptive',
-    alpha=alpha, early_stopping=validation, verbose=verbose, n_iter_no_change=iter_for_convergence)
+                        random_state=1, max_iter=epochs, learning_rate_init=learning_rate, 
+                        learning_rate='adaptive', alpha=alpha, early_stopping=validation, 
+                        verbose=verbose, n_iter_no_change=iter_for_convergence)
+
     print("Classifier initialized")
+
     # NNet training
     print("fitting starts...")
+
     clf.fit(X_train,y_train)
+
     print("fitting finished")
+
     # Some insights on the training
     array1 = clf.predict_proba(X_test[:1])
     array2 = clf.predict(X_test[:5, :])
     array3 = clf.score(X_test, y_test)
+    
     print("look for variables")
     return(clf.loss_,clf,X_test,X_train,y_test,y_train)
 
