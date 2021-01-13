7eb138bebba4406cbfaec915a4776d38bc52dcf5
Pierre Rakotondrafara <prakoton@telecom-paris.fr>
2021-01-13 12:11:46 +0100

    prepared bigger dataset and uses it in CNN

--- CNN_experimentation.py
+++ CNN_experimentation.py
@@ -7,17 +7,19 @@ import matplotlib.pyplot as plt
 import librosa, librosa.display
 from sklearn.model_selection import train_test_split
 
-verbose = False
+verbose = True
 
-np_input_path= "./numpy_material/GS_key_dataset_inputs_preprocessed.npy"
-np_label_path= "./numpy_material/GS_key_dataset_labels_preprocessed.npy"
+np_input_path= "./numpy_material/GS-mtg_key_dataset_inputs_preprocessed.npy"
+np_label_path= "./numpy_material/GS-mtg_key_dataset_labels_preprocessed.npy"
 X = np.load(np_input_path)
 y = np.load(np_label_path)
 
 if verbose:
     print("y[0] : ", y[0])
     librosa.display.specshow(X[0,:,:])
-    plt.show()
+    print(X.shape)
+    print(y.shape)
+    #plt.show()
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
 y_train = to_categorical(y_train)
@@ -26,9 +28,17 @@ y_test = to_categorical(y_test)
 X_train = X_train.reshape(-1,120,100,1)
 X_test = X_test.reshape(-1,120,100,1)
 
+if verbose: print(X_train.shape, "\n", y_train.shape, "\n")
+
 model = keras.Sequential()
-model.add(keras.Input(shape = (120,100,1)))
-for _ in range(5):
+model.add(layers.Conv2D(
+        8,
+        kernel_size=5,
+        activation="relu",
+        padding="same",
+        input_shape=(120,100,1)
+        ))
+for _ in range(4):
     model.add(layers.Conv2D(
         8,
         kernel_size=5,
@@ -36,24 +46,21 @@ for _ in range(5):
         padding="same"
         ))
 
-model.summary()
-
 model.add(layers.Permute((2,1,3)))
-
-model.summary()
 
 model.add(layers.Reshape((100,-1)))
 
 model.add(layers.Dense(48,activation="relu"))
 
-model.summary()
-
 model.add(layers.GlobalAveragePooling1D())
 
-model.summary()
+#if verbose: model.summary()
 
 model.add(layers.Dense(24,activation="softmax"))
 
 model.summary()
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=100, batch_size=64)
+
--- cnn_data_preprocessing.py
+++ cnn_data_preprocessing.py
@@ -3,12 +3,12 @@ from prepare_CNN_data import prepare_cnn_data
 
 # THIS SCRIPT CREATES THE NP ARRAYS WE WILL USE IN THE CNN AND SAVE THEM FOR LATER USE IN .npy FILES
 
-dataset_path = "./Datasets/giantsteps-key-dataset-master/audio"
-annotations_path =  "./Datasets/giantsteps-key-dataset-master/annotations/key"
+dataset_path = "./Datasets/giantsteps-mtg-key-dataset-master/audio"
+annotations_path =  "./Datasets/giantsteps-mtg-key-dataset-master/annotations/key"
 names, X, keys, y = prepare_cnn_data(dataset_path,annotations_path)
 
-outpath_X = "./numpy_material/GS_key_dataset_inputs_preprocessed"
-outpath_Y = "./numpy_material/GS_key_dataset_labels_preprocessed"
+outpath_X = "./numpy_material/GS-mtg_key_dataset_inputs_preprocessed"
+outpath_Y = "./numpy_material/GS-mtg_key_dataset_labels_preprocessed"
 
 np.save(outpath_X,X)
 np.save(outpath_Y,y)
